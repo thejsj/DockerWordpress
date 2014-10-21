f6a831b362806c60becf92a6c00374ab42b030e5
thejsj <jorge.silva@thejsj.com>
2014-10-21 00:42:21 -0400

    Fixing composer plugin types

--- wp/activate_plugins_and_themes.sh
+++ wp/activate_plugins_and_themes.sh
@@ -4,10 +4,10 @@ wp plugin activate advanced-custom-fields --allow-root
 wp plugin activate regenerate-thumbnails --allow-root
 wp plugin activate wp-migrate-db --allow-root
 wp plugin activate wp-super-cache --allow-root
-wp plugin activate http://www.colab-plugin-repository.com.php54-3.ord1-1.websitetestlink.com/plugins/acf-gallery.zip --allow-root
-wp plugin activate http://www.colab-plugin-repository.com.php54-3.ord1-1.websitetestlink.com/plugins/acf-options-page.zip --allow-root
-wp plugin activate http://www.colab-plugin-repository.com.php54-3.ord1-1.websitetestlink.com/plugins/acf-repeater.zip --allow-root
-wp plugin activate http://www.colab-plugin-repository.com.php54-3.ord1-1.websitetestlink.com/plugins/gravityforms_1.8.9.12.zip --allow-root
+wp plugin activate acf-gallery --allow-root
+wp plugin activate acf-options-page --allow-root
+wp plugin activate acf-repeater --allow-root
+wp plugin activate gravityforms --allow-root
 
 # Activate Theme
 wp theme activate cork-gulp --allow-root
